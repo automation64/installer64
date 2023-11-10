b6556b1dce28ddddb97470a2b170120cc28ba0d8
serdigital64 <serdigital64@gmail.com>
2023-11-10 19:54:20 -0300

    test: update

--- test/bash/install-bashlib64_02.sh
+++ test/bash/install-bashlib64_02.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 
-declare target="/opt/bl64"
+export INST64_BASHLIB64_TARGET="${HOME}/bl64-test"
 
 if [[ "$INST64_TEST_ENVIRONMENT" != 'ON' ]]; then
   echo 'Error: invalid test environment'
   exit 1
 fi
 
-if [[ -d "$target" ]]; then
-  sudo rm -rf "$target"
-fi
-
-sudo /opt/installer64/install-bashlib64
+/opt/installer64/install-bashlib64
--- test/bash/install-installer64_01.sh
+++ test/bash/install-installer64_01.sh
@@ -1,19 +1,14 @@
 #!/usr/bin/env bash
 
-declare bats_core_dir="/opt/bats-core"
-declare bats_file="/usr/local/bin/bats"
+declare target="/opt/installer64"
 
 if [[ "$INST64_TEST_ENVIRONMENT" != 'ON' ]]; then
   echo 'Error: invalid test environment'
   exit 1
 fi
 
-if [[ -d "$bats_core_dir" ]]; then
-  sudo rm -rf "$bats_core_dir"
-fi
-
-if [[ -L "$bats_file" ]]; then
-  sudo rm "$bats_file"
+if [[ -d "$target" ]]; then
+  sudo rm -rf "$target"
 fi
 
 sudo /opt/installer64/install-installer64
