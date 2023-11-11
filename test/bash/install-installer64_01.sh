e96601ffdd8048a16d5c1176f31a82a5a4426708
serdigital64 <serdigital64@gmail.com>
2023-11-10 22:54:21 -0300

    test: fix path

--- test/bash/install-installer64_01.sh
+++ test/bash/install-installer64_01.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 
-declare target="/opt/installer64"
+export INST64_INSTALLER64_TARGET='/opt/inst64-test'
 
 if [[ "$INST64_TEST_ENVIRONMENT" != 'ON' ]]; then
   echo 'Error: invalid test environment'
   exit 1
 fi
 
-if [[ -d "$target" ]]; then
-  sudo rm -rf "$target"
-fi
-
-sudo /opt/installer64/install-installer64
+sudo -E /opt/installer64/install-installer64
