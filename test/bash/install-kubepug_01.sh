b6654fdb0fa024544bad70431b162f187f9f43ab
serdigital64 <serdigital64@gmail.com>
2023-11-11 08:47:47 -0300

    feat: add kubepug

---
+++ test/bash/install-kubepug_01.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [[ "$INST64_TEST_ENVIRONMENT" != 'ON' ]]; then
+  echo 'Error: invalid test environment'
+  exit 1
+fi
+
+# Install requirements
+sudo /opt/installer64/install-kubectl &&
+  /opt/installer64/install-krew ||
+  exit $?
+
+/opt/installer64/install-kubepug
