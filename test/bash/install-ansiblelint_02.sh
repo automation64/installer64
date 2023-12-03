874df1cbaeb46d6af3b72bfeeac3f1f9a5ad90e4
serdigital64 <serdigital64@gmail.com>
2023-12-03 10:30:47 -0300

    test: update cases

--- test/bash/install-ansiblelint_02.sh
+++ test/bash/install-ansiblelint_02.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 
-declare test_status=0
-
 if [[ "$INST64_TEST_ENVIRONMENT" != 'ON' ]]; then
   echo 'Error: invalid test environment'
   exit 1
 fi
 
 sudo /opt/installer64/install-ansiblelint
-
-test_status=$?
-
-((test_status > 0))
