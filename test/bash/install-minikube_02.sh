5ef9aedf67021565bcade591da6b65c39eae0f9f
serdigital64 <serdigital64@gmail.com>
2023-12-19 20:03:36 -0300

    feat: add system-wide feature flag

---
+++ test/bash/install-minikube_02.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if [[ "$INST64_TEST_ENVIRONMENT" != 'ON' ]]; then
+  echo 'Error: invalid test environment'
+  exit 1
+fi
+
+export INST64_MINIKUBE_SYSTEM_WIDE='NO'
+export INST64_MINIKUBE_TARGET="${HOME}/test"
+
+/opt/installer64/install-minikube
