cdd88316b9b26dbea18be09f7a57e27ca14c1a61
serdigital64 <serdigital64@gmail.com>
2024-04-01 09:25:52 +0000

    doc: update templates

--- docs/snippets/installer/install-python.bash
+++ docs/snippets/installer/install-python.bash
@@ -1,14 +1,25 @@
-# Snippet: install-python-3.1.0
+# Snippet: install-python-3.2.0
+
+# X_IMPORTS_PLACEHOLDER_X
+# shellcheck source-path=lib/bl64 disable=SC2015
+source "${INST64_BASHLIB64}/bashlib64-module-py.bash" &&
+  source "${INST64_BASHLIB64}/bashlib64-module-txt.bash" &&
+  source "${INST64_BASHLIB64}/bashlib64-module-fmt.bash" &&
+  source "${INST64_BASHLIB64}/bashlib64-module-fs.bash" &&
+  source "${INST64_BASHLIB64}/bashlib64-core.bash" ||
 
 # X_GLOBALS_PLACEHOLDER_X
+export INST64_X_APP_NAME_CAPS_X_VERSION="${INST64_X_APP_NAME_CAPS_X_VERSION:-latest}"
 # Installation method
 export INST64_X_APP_NAME_CAPS_X_METHOD="${INST64_X_APP_NAME_CAPS_X_METHOD:-PIP}"
-export INST64_X_APP_NAME_CAPS_X_VERSION="${INST64_X_APP_NAME_CAPS_X_VERSION:-latest}"
+
 export INST64_X_APP_NAME_CAPS_X_PIPX_BIN='/usr/bin/pipx'
 
 # X_STAND_ALONE_FUNCTIONS_X #
 function inst64_X_APP_NAME_X_install_with_pip() {
   bl64_dbg_app_show_function
+
+  bl64_msg_show_task 'deploy application'
   INST64_X_APP_NAME_CAPS_X_CLI_PATH="${HOME}/.local/bin/${INST64_X_APP_NAME_CAPS_X_CLI_NAME}"
   bl64_fs_set_umask "$BL64_FS_UMASK_RW_USER_RO_ALL" &&
     bl64_py_pip_usr_prepare &&
@@ -17,12 +28,13 @@ function inst64_X_APP_NAME_X_install_with_pip() {
 
 function inst64_X_APP_NAME_X_install_with_pipx() {
   bl64_dbg_app_show_function
+
+  bl64_msg_show_task 'deploy application'
   INST64_X_APP_NAME_CAPS_X_CLI_PATH="${INST64_X_APP_NAME_CAPS_X_CLI_NAME}"
-  pipx install "$INST64_X_APP_NAME_CAPS_X_PACKAGES"
+  "$INST64_X_APP_NAME_CAPS_X_PIPX_BIN" install "$INST64_X_APP_NAME_CAPS_X_PACKAGES"
 }
 
 # X_INSTALL_PLACEHOLDER_X
-  bl64_msg_show_task 'deploy application'
   if [[ "$INST64_X_APP_NAME_CAPS_X_METHOD" == 'PIPX' ]]; then
     inst64_X_APP_NAME_X_install_with_pipx
   elif [[ "$INST64_X_APP_NAME_CAPS_X_METHOD" == 'PIP' ]]; then
@@ -46,16 +58,22 @@ function inst64_X_APP_NAME_X_install_with_pipx() {
   fi
 
 # X_PREPARE_PLACEHOLDER_X
-  bl64_py_setup
+  if [[ "$INST64_X_APP_NAME_CAPS_X_METHOD" == 'PIPX' || "$INST64_X_APP_NAME_CAPS_X_METHOD" == 'PIP' ]]; then
+    bl64_py_setup
+  fi
 
 # X_INIT_PLACEHOLDER_X
-  bl64_os_check_version \
-    "$X_BL64_OS_ID_X" &&
-    bl64_fmt_check_value_in_list 'invalid installation method for the parameter INST64_X_APP_NAME_CAPS_X_METHOD' \
-      "$INST64_X_APP_NAME_CAPS_X_METHOD" \
-      'PIP' 'PIPX' &&
-    bl64_check_privilege_not_root ||
+  bl64_fmt_check_value_in_list 'invalid installation method for the parameter INST64_X_APP_NAME_CAPS_X_METHOD' \
+    "$INST64_X_APP_NAME_CAPS_X_METHOD" \
+    'PIP' 'PIPX' ||
     return $?
-  if [[ "$INST64_X_APP_NAME_CAPS_X_METHOD" == 'PIPX' ]]; then
-    bl64_check_command "$INST64_X_APP_NAME_CAPS_X_PIPX_BIN"
+
+  if [[ "$INST64_X_APP_NAME_CAPS_X_METHOD" == 'PIPX' || "$INST64_X_APP_NAME_CAPS_X_METHOD" == 'PIP' ]]; then
+    if [[ "$INST64_X_APP_NAME_CAPS_X_METHOD" == 'PIPX' ]]; then
+      bl64_check_command "$INST64_X_APP_NAME_CAPS_X_PIPX_BIN" ||
+      return $?
+    fi
+    bl64_check_privilege_not_root &&
+      bl64_os_check_compatibility \
+        "${BL64_OS_X_OS_TAG_X}-X_OS_VERSION_X"
   fi
