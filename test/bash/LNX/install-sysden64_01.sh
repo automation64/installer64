33f70985573005666e38df1ad119871ac3f401a4
SerDigital64 <serdigital64@gmail.com>
2024-07-09 11:34:16 +0000

    test: fix

--- test/bash/LNX/install-sysden64_01.sh
+++ test/bash/LNX/install-sysden64_01.sh
@@ -5,6 +5,6 @@ if [[ "$_DEV_TEST_BASH_CONTAINER_ENVIRONMENT" != 'ON' ]]; then
   exit 1
 fi
 
-export INST64_SYSOP64_SYSTEM_WIDE=NO
-export INST64_SYSOP64_TARGET="$HOME/sysden64"
+export INST64_SYSDEN64_SYSTEM_WIDE=NO
+export INST64_SYSDEN64_TARGET="$HOME/sysden64"
 /source/install-sysden64
