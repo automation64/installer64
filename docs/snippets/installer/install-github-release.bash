1475fde86607c0ed633c16cfc04b6762a6ad9cea
SerDigital64 <serdigital64@gmail.com>
2024-07-10 20:45:30 +0200

    doc: update template

--- docs/snippets/installer/install-github-release.bash
+++ docs/snippets/installer/install-github-release.bash
@@ -1,8 +1,10 @@
-# Snippet: install-github-release-3.2.3
+# Snippet: install-github-release-3.2.4
 
 # X_IMPORTS_PLACEHOLDER_X
 # shellcheck source-path=lib/bl64 disable=SC2015
-source "${INST64_BASHLIB64}/bashlib64-module-rxtx.bash" &&
+source "${INST64_BASHLIB64}/bashlib64-module-xsv.bash" &&
+  source "${INST64_BASHLIB64}/bashlib64-module-bsh.bash" &&
+  source "${INST64_BASHLIB64}/bashlib64-module-rxtx.bash" &&
   source "${INST64_BASHLIB64}/bashlib64-module-api.bash" &&
   source "${INST64_BASHLIB64}/bashlib64-module-vcs.bash" &&
   source "${INST64_BASHLIB64}/bashlib64-module-txt.bash" &&
