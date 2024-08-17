db7937ff21f4f8bd91620934e418f81e4da741cd
serdigital64 <serdigital64@gmail.com>
2024-08-17 08:10:38 +0000

    test: update

--- test/bash/ALP/install-mkdocs_02.sh
+++ test/bash/ALP/install-mkdocs_02.sh
@@ -5,6 +5,10 @@ if [[ "$_DEV_TEST_BASH_CONTAINER_ENVIRONMENT" != 'ON' ]]; then
   exit 1
 fi
 
+# Prepare PIPX
+export PATH="$PATH:/home/$HOME/.local/bin"
 /source/install-pipx
+
+# Run test
 export INST64_MKDOCS_METHOD='PIPX'
 /source/install-mkdocs
