ba07a3fc91dcbdb45d10f6bcf4d2f131e8905900
serdigital64 <serdigital64@gmail.com>
2024-08-31 18:12:35 +0000

    test: update

--- test/bash/UB/install-pgadmin_01.sh
+++ test/bash/UB/install-pgadmin_01.sh
@@ -2,7 +2,7 @@
 
 export INST64_ANSIBLE_PIPX="OFF"
 
-if [[ "$_DEV_TEST_BASH_CONTAINER_ENVIRONMENT" != 'ON' ]]; then
+if [[ "$DEV_TEST_BASH_CONTAINER_ENVIRONMENT" != 'ON' ]]; then
   echo 'Error: invalid test environment'
   exit 1
 fi
