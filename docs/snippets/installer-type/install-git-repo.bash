ed62b7b6653b2c349001baf71bf1e96e9508abc2
serdigital64 <serdigital64@gmail.com>
2024-09-01 10:35:24 +0000

    doc: update

--- docs/snippets/installer-type/install-git-repo.bash
+++ docs/snippets/installer-type/install-git-repo.bash
@@ -29,6 +29,7 @@ function inst64_X_APP_NAME_X_install_git_repo() {
   bl64_dbg_app_show_function
   local app_target_mode='0755'
   local app_target_owner='root'
+  local app_cli_source="${INST64_X_APP_NAME_CAPS_X_CLI_NAME}"
 
   if bl64_lib_flag_is_enabled "$INST64_X_APP_NAME_CAPS_X_SYSTEM_WIDE"; then
     INST64_X_APP_NAME_CAPS_X_CLI_PATH="${INST64_LOCAL_BIN}/${INST64_X_APP_NAME_CAPS_X_CLI_NAME}"
@@ -60,7 +61,7 @@ function inst64_X_APP_NAME_X_install_git_repo() {
   if bl64_lib_flag_is_enabled "$INST64_X_APP_NAME_CAPS_X_SYSTEM_WIDE"; then
     bl64_msg_show_task "publish application to searchable path (${INST64_X_APP_NAME_CAPS_X_CLI_PATH})"
     # shellcheck disable=SC2086
-    bl64_fs_create_symlink "${INST64_X_APP_NAME_CAPS_X_TARGET}/${INST64_X_APP_NAME_CAPS_X_CLI_NAME}" "$INST64_X_APP_NAME_CAPS_X_CLI_PATH" "$BL64_VAR_ON" ||
+    bl64_fs_create_symlink "${INST64_X_APP_NAME_CAPS_X_TARGET}/${app_cli_source}" "$INST64_X_APP_NAME_CAPS_X_CLI_PATH" "$BL64_VAR_ON" ||
       return $?
   fi
 
