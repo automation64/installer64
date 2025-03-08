b3848aadf8bfb636d54dcb6961ff0c46d7dbebfc
SerDigital64 <serdigital64@gmail.com>
2025-03-08 19:04:44 +0000

    feat: add installer

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -21,6 +21,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 - Installers
   - UV
+  - Terraform Docs
 - Trivy
   - Multi Machine support
 - IstioCTL
---
+++ test/bash/LNX/install-terraform-docs_01.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if [[ "$DEV_TEST_BASH_CONTAINER_ENVIRONMENT" != 'ON' ]]; then
+  echo 'Error: invalid test environment'
+  exit 1
+fi
+
+export INST64_SYSTEM_WIDE='YES'
+sudo -E /source/install-terraform-docs
