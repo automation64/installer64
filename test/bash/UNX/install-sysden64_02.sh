02dc35ab855d022992c795aa0fec3cf98eac0b9c
X_GIT_USER_NAME_X <X_GIT_USER_EMAIL_X>
2025-05-25 10:22:25 +0000

    test: fix

--- test/bash/UNX/install-sysden64_02.sh
+++ test/bash/UNX/install-sysden64_02.sh
@@ -2,5 +2,9 @@
 
 source test/lib/check.bash
 export INST64_SYSTEM_WIDE='YES'
+
+if [[ -d $HOME/at64 ]]; then
+    rm -Rf $HOME/at64
+fi
 sudo -E src/install-installer64
 sudo -E src/install-sysden64
