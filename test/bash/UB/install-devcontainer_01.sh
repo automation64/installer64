5fbeee3435c761cd4c30111c96cc480d897b3a62
SerDigital64 (Max) <serdigital64@gmail.com>
2025-05-25 12:46:08 +0200

    feat: promote 5.11.0 (#120)

--- test/bash/UB/install-devcontainer_01.sh
+++ test/bash/UB/install-devcontainer_01.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+PROJECT="$(pwd)"
 export INST64_ANSIBLELINT_PIPX="OFF"
 
 source test/lib/check.bash
 export INST64_SYSTEM_WIDE='YES'
 sudo -E src/install-nodejs ||
   return $?
-cd $HOME &&
-  src/install-devcontainer
+export INST64_SYSTEM_WIDE='NO'
+cd "$HOME" &&
+  "$PROJECT/src/install-devcontainer"
