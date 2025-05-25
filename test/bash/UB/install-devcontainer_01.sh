77a38b92727d4bf1aaee7bb32fe4a6c1fa4d6622
X_GIT_USER_NAME_X <X_GIT_USER_EMAIL_X>
2025-05-25 10:08:58 +0000

    test: fix

--- test/bash/UB/install-devcontainer_01.sh
+++ test/bash/UB/install-devcontainer_01.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
+PROJECT="$(pwd)"
 export INST64_ANSIBLELINT_PIPX="OFF"
 
 source test/lib/check.bash
 export INST64_SYSTEM_WIDE='YES'
 sudo -E src/install-nodejs ||
   return $?
-cd $HOME &&
-  src/install-devcontainer
+cd "$HOME" &&
+  "$PROJECT/src/install-devcontainer"
