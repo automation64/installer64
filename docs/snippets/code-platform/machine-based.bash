865d95c8de2a89a60ad1df73aedd57800e70eb85
X_GIT_USER_NAME_X <X_GIT_USER_EMAIL_X>
2025-05-31 12:12:08 +0000

    doc: update

--- docs/snippets/code-platform/machine-based.bash
+++ docs/snippets/code-platform/machine-based.bash
@@ -1,4 +1,5 @@
   # X_PLATFORM_SELECTION_PLACEHOLDER_X #
+
       if [[ "$BL64_OS_TYPE" == "$BL64_OS_TYPE_MACOS" && "$BL64_OS_MACHINE" == "$BL64_OS_MACHINE_ARM64" ]]; then
         INST64_X_APP_NAME_CAPS_X_PLATFORM='X_PLATFORM_TAG_X'
       elif [[ "$BL64_OS_TYPE" == "$BL64_OS_TYPE_LINUX" && "$BL64_OS_MACHINE" == "$BL64_OS_MACHINE_AMD64" ]]; then
@@ -7,6 +8,8 @@
         bl64_msg_show_error "unsupported OS and Machine combination (OS_TYPE:${BL64_OS_TYPE} / MACHINE: ${BL64_OS_MACHINE})"
         return $BL64_LIB_ERROR_OS_INCOMPATIBLE
       fi
+
+# Alternative Machine
 
       if [[ "$BL64_OS_TYPE" == "$BL64_OS_TYPE_MACOS" && "$BL64_OS_MACHINE" == "$BL64_OS_MACHINE_ARM64" ]]; then
         INST64_X_APP_NAME_CAPS_X_PLATFORM='darwin/arm64'
--- docs/snippets/method-binary-custom/install.bash
+++ docs/snippets/method-binary-custom/install.bash
@@ -1,15 +1,14 @@
 function inst64_X_APP_NAME_X_install_binary() {
   bl64_dbg_app_show_function
   local work_path=''
-  local app_work_path="${INST64_CLI_NAME}"
 
   bl64_msg_show_task 'download application'
   work_path="$(bl64_fs_create_tmpdir)" || return $?
   bl64_rxtx_web_get_file \
-    "${INST64_WEB_REPO_URL}/${INST64_PKG_MAIN}" \
+    "${INST64_WEB_REPO_URL}/${INST64_WEB_REPO_PATH}/${INST64_PKG_MAIN}" \
     "${work_path}/${INST64_PKG_MAIN}" \
     'YES' \
-    "$app_target_mode" &&
+    "$INST64_TARGET_MODE" &&
   bl64_arc_open_tar "${work_path}/${INST64_PKG_MAIN}" "${work_path}" ||
   return $?
 
