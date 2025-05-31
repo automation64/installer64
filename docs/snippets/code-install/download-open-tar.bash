b589b59d98de3d15bc268bc5895b9a52f013ea92
X_GIT_USER_NAME_X <X_GIT_USER_EMAIL_X>
2025-05-31 10:12:24 +0000

    refact: normalize var names

--- docs/snippets/code-install/download-open-tar.bash
+++ docs/snippets/code-install/download-open-tar.bash
@@ -1,3 +1,3 @@
 # X_INSTALL_DOWNLOAD_PLACEHOLDER_X
-    bl64_arc_open_tar "${work_path}/${INST64_APP_PACKAGES}" "${work_path}" ||
+    bl64_arc_open_tar "${work_path}/${INST64_PKG_MAIN}" "${work_path}" ||
     return $?
