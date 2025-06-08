56244fbc45a065af2de7e405776a1c14a7184518
SerDigital64 (Max) <serdigital64@gmail.com>
2025-06-08 13:50:29 +0200

    dev: update cicd (#122)

--- test/bash/UNX/install-installer64_02.sh
+++ test/bash/UNX/install-installer64_02.sh
@@ -2,4 +2,5 @@
 
 source test/lib/check.bash
 export INST64_SYSTEM_WIDE=NO
+export INST64_REPLACE_INSTALLED='YES'
 src/install-installer64
--- test/bash/UNX/install-sysop64_01.sh
+++ test/bash/UNX/install-sysop64_01.sh
@@ -2,5 +2,6 @@
 
 source test/lib/check.bash
 export INST64_SYSTEM_WIDE=NO
+export INST64_REPLACE_INSTALLED='YES'
 src/install-installer64
 src/install-sysop64
