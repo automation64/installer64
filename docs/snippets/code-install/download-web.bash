a4ddbafcb69ba754b0592572e0816f213b104818
X_GIT_USER_NAME_X <X_GIT_USER_EMAIL_X>
2025-06-13 06:20:11 +0000

    doc: update

---
+++ docs/snippets/code-install/download-web.bash
@@ -0,0 +1,5 @@
+    bl64_rxtx_web_get_file \
+      "${INST64_WEB_REPO_URL}/${INST64_PKG_MAIN}" \
+      "${work_path}/${INST64_PKG_MAIN}" \
+      'YES' \
+      "$app_target_mode" ||
--- docs/snippets/code-platform/machine-based.bash
+++ docs/snippets/code-platform/machine-based.bash
@@ -4,9 +4,6 @@
         INST64_X_APP_NAME_CAPS_X_PLATFORM='X_PLATFORM_TAG_X'
       elif [[ "$BL64_OS_TYPE" == "$BL64_OS_TYPE_LINUX" && "$BL64_OS_MACHINE" == "$BL64_OS_MACHINE_AMD64" ]]; then
         INST64_X_APP_NAME_CAPS_X_PLATFORM='X_PLATFORM_TAG_X'
-      else
-        bl64_msg_show_error "unsupported OS and Machine combination (OS_TYPE:${BL64_OS_TYPE} / MACHINE: ${BL64_OS_MACHINE})"
-        return $BL64_LIB_ERROR_OS_INCOMPATIBLE
       fi
 
 # Alternative Machine
@@ -15,9 +12,6 @@
         INST64_X_APP_NAME_CAPS_X_PLATFORM='darwin/arm64'
       elif [[ "$BL64_OS_TYPE" == "$BL64_OS_TYPE_LINUX" && "$BL64_OS_MACHINE" == "$BL64_OS_MACHINE_AMD64" ]]; then
         INST64_X_APP_NAME_CAPS_X_PLATFORM='linux/amd64'
-      else
-        inst64_lib_alert_incompatible_platform
-        return $?
       fi
 
 # Alternative Machine
@@ -26,9 +20,6 @@
         INST64_X_APP_NAME_CAPS_X_PLATFORM='Darwin_arm64'
       elif [[ "$BL64_OS_TYPE" == "$BL64_OS_TYPE_LINUX" && "$BL64_OS_MACHINE" == "$BL64_OS_MACHINE_AMD64" ]]; then
         INST64_X_APP_NAME_CAPS_X_PLATFORM='Linux_amd64'
-      else
-        inst64_lib_alert_incompatible_platform
-        return $?
       fi
 
 # Alternative Machine
@@ -37,9 +28,6 @@
         INST64_X_APP_NAME_CAPS_X_PLATFORM='macOS_amd64'
       elif [[ "$BL64_OS_TYPE" == "$BL64_OS_TYPE_LINUX" && "$BL64_OS_MACHINE" == "$BL64_OS_MACHINE_AMD64" ]]; then
         INST64_X_APP_NAME_CAPS_X_PLATFORM='linux_amd64'
-      else
-        inst64_lib_alert_incompatible_platform
-        return $?
       fi
 
 # Alternative Machine
@@ -48,9 +36,6 @@
         INST64_X_APP_NAME_CAPS_X_PLATFORM='darwin_arm64'
       elif [[ "$BL64_OS_TYPE" == "$BL64_OS_TYPE_LINUX" && "$BL64_OS_MACHINE" == "$BL64_OS_MACHINE_AMD64" ]]; then
         INST64_X_APP_NAME_CAPS_X_PLATFORM='linux_amd64'
-      else
-        inst64_lib_alert_incompatible_platform
-        return $?
       fi
 
 # Alternative Machine
@@ -59,9 +44,6 @@
         INST64_X_APP_NAME_CAPS_X_PLATFORM='darwin-arm64'
       elif [[ "$BL64_OS_TYPE" == "$BL64_OS_TYPE_LINUX" && "$BL64_OS_MACHINE" == "$BL64_OS_MACHINE_AMD64" ]]; then
         INST64_X_APP_NAME_CAPS_X_PLATFORM='linux-amd64'
-      else
-        inst64_lib_alert_incompatible_platform
-        return $?
       fi
 
 # Alternative Machine
@@ -70,9 +52,6 @@
         INST64_X_APP_NAME_CAPS_X_PLATFORM='macos-arm64'
       elif [[ "$BL64_OS_TYPE" == "$BL64_OS_TYPE_LINUX" && "$BL64_OS_MACHINE" == "$BL64_OS_MACHINE_AMD64" ]]; then
         INST64_X_APP_NAME_CAPS_X_PLATFORM='linux-x86_64'
-      else
-        inst64_lib_alert_incompatible_platform
-        return $?
       fi
 
 # Alternative Machine
@@ -81,7 +60,4 @@
         INST64_X_APP_NAME_CAPS_X_PLATFORM='darwin-arm64'
       elif [[ "$BL64_OS_TYPE" == "$BL64_OS_TYPE_LINUX" && "$BL64_OS_MACHINE" == "$BL64_OS_MACHINE_AMD64" ]]; then
         INST64_X_APP_NAME_CAPS_X_PLATFORM='linux-x64'
-      else
-        inst64_lib_alert_incompatible_platform
-        return $?
       fi
