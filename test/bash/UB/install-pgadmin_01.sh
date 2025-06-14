459ac7f7b36c936f229560d9e64c95de6be9d929
SerDigital64 (Max) <serdigital64@gmail.com>
2025-06-14 11:33:49 +0200

    real: promote 6.0.4 (#126)

--- test/bash/UB/install-pgadmin_01.sh
+++ test/bash/UB/install-pgadmin_01.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-export INST64_ANSIBLE_PIPX="OFF"
-
 source test/lib/check.bash
 # Prepare PIPX
 export PATH="${PATH}:${HOME}/.local/bin"
--- test/bash/UB/install-pgcli_01.sh
+++ test/bash/UB/install-pgcli_01.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-export INST64_ANSIBLE_PIPX="OFF"
-
 source test/lib/check.bash
 src/install-pgcli
