a55c883fea95d99efda88c97d72fdf2bb4ca0459
X_GIT_USER_NAME_X <X_GIT_USER_EMAIL_X>
2025-06-21 16:44:54 +0000

    test: update

--- test/bash/UB/install-mkdocs_01.sh
+++ test/bash/UB/install-mkdocs_01.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 source test/lib/check.bash
+
 # Prepare PIPX
 export PATH="${PATH}:${HOME}/.local/bin"
 if python3 -c "import sys; sys.exit(0 if sys.version_info >= (3, 12) else 1)"; then
