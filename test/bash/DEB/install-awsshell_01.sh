1fe8591c462887df8f61bfb00f2667663740ff6f
X_GIT_USER_NAME_X <X_GIT_USER_EMAIL_X>
2025-06-21 10:38:43 +0000

    test: update

--- test/bash/DEB/install-awsshell_01.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-source test/lib/check.bash
-src/install-awsshell
--- test/bash/OEL/install-awsshell_01.sh
+++ test/bash/OEL/install-awsshell_01.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 source test/lib/check.bash
+export PATH="${PATH}:${HOME}/.local/bin"
 src/install-pipx
 src/install-awsshell
