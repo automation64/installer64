6345ba9206adbe06ad43f12f766c5efe5cbe6d98
X_GIT_USER_NAME_X <X_GIT_USER_EMAIL_X>
2025-06-28 15:44:08 +0000

    test: update

--- test/bash/UNX/install-devbin64_02.sh
+++ test/bash/UNX/install-devbin64_02.sh
@@ -3,8 +3,7 @@
 source test/lib/check.bash
 export INST64_SYSTEM_WIDE='YES'
 export INST64_REPLACE_INSTALLED='YES'
-if [[ -d $HOME/at64 ]]; then
-    rm -Rf $HOME/at64
-fi
+rm -rf $HOME/at64 2> /dev/null
+sudo rm -Rf /root/at64 2> /dev/null
 sudo -E src/install-installer64
 sudo -E src/install-devbin64
--- test/bash/UNX/install-sysden64_02.sh
+++ test/bash/UNX/install-sysden64_02.sh
@@ -4,8 +4,7 @@ source test/lib/check.bash
 export INST64_SYSTEM_WIDE='YES'
 export INST64_REPLACE_INSTALLED='YES'
 
-if [[ -d $HOME/at64 ]]; then
-    rm -Rf $HOME/at64
-fi
+rm -Rf $HOME/at64 2> /dev/null
+sudo rm -Rf /root/at64 2> /dev/null
 sudo -E src/install-installer64
 sudo -E src/install-sysden64
