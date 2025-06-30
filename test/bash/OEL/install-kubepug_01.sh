9645825498802c89c3a0b18749520665faa2c7b7
X_GIT_USER_NAME_X <X_GIT_USER_EMAIL_X>
2025-06-30 19:28:11 +0000

    test: update

--- test/bash/OEL/install-kubepug_01.sh
+++ test/bash/OEL/install-kubepug_01.sh
@@ -4,8 +4,8 @@ source test/lib/test.bash
 # Install requirements
 export PATH="${HOME}/.krew/bin:$PATH"
 export INST64_SYSTEM_WIDE='YES'
-sudo -E "${DEV_ENV_BASH_CONTAINER_PATH_SRC}/"install-kubectl &&
-  "${DEV_ENV_BASH_CONTAINER_PATH_SRC}/"install-krew ||
-  exit $?
+sudo -E "${DEV_ENV_BASH_CONTAINER_PATH_SRC}/"install-kubectl
+export INST64_SYSTEM_WIDE='NO'
+"${DEV_ENV_BASH_CONTAINER_PATH_SRC}/"install-krew
 
 "${DEV_ENV_BASH_CONTAINER_PATH_SRC}/"install-kubepug
