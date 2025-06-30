c91fa2227c6376f8715ef52faa47a486f072df89
X_GIT_USER_NAME_X <X_GIT_USER_EMAIL_X>
2025-06-30 19:08:58 +0000

    test: update

--- test/bash/UNX/install-sysden64_01.sh
+++ test/bash/UNX/install-sysden64_01.sh
@@ -3,4 +3,5 @@
 source test/lib/test.bash
 export INST64_SYSTEM_WIDE=NO
 export INST64_REPLACE_INSTALLED='YES'
+export SYSDEN64_PATH_BL64="$INST64_BASHLIB64"
 "${DEV_ENV_BASH_CONTAINER_PATH_SRC}/"install-sysden64
