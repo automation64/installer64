dca38064d00b6757f3fcc4e9c8652956dfffc1b9
X_GIT_USER_NAME_X <X_GIT_USER_EMAIL_X>
2025-07-04 13:44:06 +0000

    test: update

--- test/bash/UB/install-ray_01.sh
+++ test/bash/UB/install-ray_01.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-export INST64_ANSIBLELINT_PIPX="OFF"
-
 source test/lib/test.bash
 if python3 -c "import sys; sys.exit(0 if sys.version_info >= (3, 12) else 1)"; then
     sudo src/install-pipx
