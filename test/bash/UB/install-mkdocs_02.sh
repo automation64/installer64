27114da5767c70e8e1f5f0125bcc099a9f80a141
X_GIT_USER_NAME_X <X_GIT_USER_EMAIL_X>
2025-07-05 11:31:57 +0000

    test: update

--- test/bash/UB/install-mkdocs_02.sh
+++ test/bash/UB/install-mkdocs_02.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
 source test/lib/test.bash
+# Prepare PIPX
+export PATH="${PATH}:${HOME}/.local/bin"
+if python3 -c "import sys; sys.exit(0 if sys.version_info >= (3, 12) else 1)"; then
+    sudo src/install-pipx
+else
+    src/install-pipx
+fi
 src/install-mkdocs
