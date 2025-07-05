df04296ed04f8e0f1e4929594ba2640c83272c44
X_GIT_USER_NAME_X <X_GIT_USER_EMAIL_X>
2025-07-05 09:48:33 +0000

    refact. Upgrade to lib bl64

---
+++ test/adhoc/test-lib.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+source test/lib/test.bash
+
+source "${INST64_BASHLIB64}/bashlib64-module-txt.bash" &&
+  source "${INST64_BASHLIB64}/bashlib64-module-fmt.bash" &&
+  source "${INST64_BASHLIB64}/bashlib64-module-rnd.bash" &&
+  source "${INST64_BASHLIB64}/bashlib64-module-iam.bash" &&
+  source "${INST64_BASHLIB64}/bashlib64-core.bash" || exit 1
+
+source src/lib-bl64
