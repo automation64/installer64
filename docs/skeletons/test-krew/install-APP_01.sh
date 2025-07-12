bc1727640715ba2166c1f480d710fdbd5dcf1552
X_GIT_USER_NAME_X <X_GIT_USER_EMAIL_X>
2025-07-12 08:09:33 +0000

    doc: update

---
+++ docs/skeletons/test-krew/install-APP_01.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+source test/lib/test.bash
+
+export PATH="${HOME}/.krew/bin:$PATH"
+export INST64_SYSTEM_WIDE='YES'
+sudo -E src/install-kubectl
+export INST64_SYSTEM_WIDE='NO'
+src/install-krew
+
+src/install-APP
