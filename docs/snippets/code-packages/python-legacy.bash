83b8a78911e33d82f4c81c2b0087311cd98dd3df
SerDigital64 (Max) <serdigital64@gmail.com>
2025-07-12 14:41:13 +0200

    real: promote 6.2.0 (#138)

--- docs/snippets/code-packages/python-legacy.bash
+++
@@ -1,7 +0,0 @@
-# X_SELECT_PKG_PLACEHOLDER_X
-  local version_legacy='X_LEGACY_VERSION_X'
-
-    if bl64_os_is_distro "${X_BL64_OS_ID_X}"; then
-      bl64_msg_show_alert "fixing package version_target for compatibility to target OS (${version_legacy})"
-      version_target="==${version_legacy}.*"
-    elif [[ "$INST64_X_APP_NAME_CAPS_X_VERSION" == 'latest' ]]; then
--- docs/snippets/method-native-external/prepare.bash
+++
@@ -1,3 +0,0 @@
-  if [[ "$INST64_X_APP_NAME_CAPS_X_METHOD" == 'NATIVE' ]]; then
-    inst64_prepare_repository
-  fi
