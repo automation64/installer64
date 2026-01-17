207fe4b1bdd4eb7591c086a9757566def530d0d6
bl64_vcs_run_git <nouser@nodomain>
2026-01-17 12:49:54 +0100

    real: promote 7.7.0

--- test/bash/LNX/install-bashit_01.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-source test/lib/test.bash
-src/install-bashit
--- test/bash/LNX/install-zsh_01.sh
+++ test/bash/LNX/install-zsh_01.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 source test/lib/test.bash
+
 export INST64_SYSTEM_WIDE='YES'
-sudo -E src/install-hwatch
+sudo -E src/install-zsh
