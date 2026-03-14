1c3a59a36278e1daeeae9b9a9b94d9a03ec13e58
SerDigital64 <serdigital64@gmail.com>
2026-03-14 22:25:55 +0100

    test: optimize

--- test/bash/UB/install-ray_01.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-source test/lib/test.bash
-if python3 -c "import sys; sys.exit(0 if sys.version_info >= (3, 12) else 1)"; then
-    sudo src/install-pipx
-else
-    src/install-pipx
-fi
-src/install-ray
