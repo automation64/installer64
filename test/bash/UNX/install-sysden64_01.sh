88ca90fe8993651439bc77d89ba14da782d269c2
SerDigital64 (Max) <serdigital64@gmail.com>
2026-06-06 16:43:14 +0200

    real: promote 10.0.0 (#184)

--- test/bash/UNX/install-sysden64_01.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-source test/lib/test.bash
-export INST64_SYSTEM_WIDE=NO
-export INST64_REPLACE_INSTALLED='YES'
-export SYSDEN64_PATH_BL64="$INST64_BASHLIB64"
-src/install-sysden64
--- test/bash/UNX/lib-bl64_01.sh
+++ test/bash/UNX/lib-bl64_01.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+export BL64_SCRIPT_PATH='src'
 source src/lib-bl64
